345f4a354b4625ed55018b6105b525f4a1c0edc2
alandliu <52473359+alandliu@users.noreply.github.com>
2021-10-13 16:40:32 -0700

    Add files via upload

---
+++ playerControllers.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerControllers : MonoBehaviour
+{
+
+    public int speed = 5;
+    public float jumpForce = 5f;
+    public Rigidbody2D rb;
+    private float movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+        }
+
+        rb.position = rb.position + new Vector2(movement * speed * Time.deltaTime, 0);
+    }
+
+
+}
